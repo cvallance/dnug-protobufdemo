1eaa9ca3e7d2a479eadbf7d0abaa0b26d8de79e4
Charles Vallance <vallance.charles@gmail.com>
2018-09-19 14:54:10 +1200

    Simple writing and reading demo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
 
 namespace DNUG.ProtobufDemoComplete
 {
@@ -8,7 +10,29 @@ namespace DNUG.ProtobufDemoComplete
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            var garry = new Person
+            {
+                Id = 1245,
+                Name = "Garry Garryson",
+                Email = "garry@garrysoncorp.com",
+                Phones =
+                {
+                    new Person.Types.PhoneNumber {Number = "555 1234 5678", Type = Person.Types.PhoneType.Home},
+                    new Person.Types.PhoneNumber {Number = "555 1234 8765", Type = Person.Types.PhoneType.Work}
+                },
+                LastUpdated = Timestamp.FromDateTime(DateTime.UtcNow)
+            };
+
+            using (var output = File.Create("garry.dat"))
+            {
+                garry.WriteTo(output);
+            }
+
+            using (var input = File.OpenRead("garry.dat"))
+            {
+                var garryAgain = Person.Parser.ParseFrom(input);
+                Console.WriteLine($"Itttttssss {garryAgain.Name}");
+            }
         }
     }
 }
