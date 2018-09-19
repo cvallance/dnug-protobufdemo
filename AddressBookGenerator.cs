8b47db19c99ee62deb6a6831afdc00f613cf60ea
Charles Vallance <vallance.charles@gmail.com>
2018-09-19 15:26:35 +1200

    Large data generation, saving and loading. JSON comparison included... but don't read too much into the times.

---
+++ AddressBookGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Bogus;
+using Google.Protobuf.WellKnownTypes;
+
+namespace DNUG.ProtobufDemoComplete
+{
+    public static class AddressBookGenerator
+    {
+        private const short NumberOfPeople = 50;
+        private const byte NumberOfPhoneNums = 2;
+        public static AddressBook Generate()
+        {
+            var people = GeneratePeople();
+            var addressBook = new AddressBook();
+            addressBook.People.Add(people);
+
+            return addressBook;
+        }
+
+        private static IEnumerable<Person> GeneratePeople()
+        {
+            var lastUpdated = Timestamp.FromDateTime(DateTime.UtcNow);
+            var personFaker = new Faker<Person>()
+                .RuleFor(x => x.Name, f => f.Person.FullName)
+                .RuleFor(x => x.Email, f => f.Person.Email)
+                .RuleFor(x => x.LastUpdated, lastUpdated);
+            
+            var phoneNumberFaker = new Faker<Person.Types.PhoneNumber>()
+                .RuleFor(x => x.Number, f => f.Phone.PhoneNumber())
+                .RuleFor(x => x.Type, f => f.PickRandom<Person.Types.PhoneType>());
+            
+            for (var i = 0; i < NumberOfPeople; i++)
+            {
+                var person = personFaker.Generate();
+                var phoneNumbers = phoneNumberFaker.Generate(NumberOfPhoneNums);
+                person.Phones.Add(phoneNumbers);
+                
+                yield return person;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,21 +3,28 @@ using System.Diagnostics;
 using System.IO;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
+using Newtonsoft.Json;
 
 namespace DNUG.ProtobufDemoComplete
 {
     class Program
     {
         private const string GarryFileName = "garry.dat";
+        private const string AddressBookFileName = "addressbook.dat";
+        private const string AddressBookJsonFileName = "addressbook.json";
         
         static void Main(string[] args)
         {
-            SimpleGarryWrite();
-            SimpleGarryRead();
+//            SimpleGarryWrite();
+//            SimpleGarryRead();
+//            SaveAddressBook();
+//            ReadAddressBook();
         }
 
         private static void SimpleGarryWrite()
         {
+            Console.WriteLine("SimpleGarryWrite");
+            
             var garry = new Person
             {
                 Id = 1245,
@@ -41,6 +48,8 @@ namespace DNUG.ProtobufDemoComplete
         
         private static void SimpleGarryRead()
         {
+            Console.WriteLine("SimpleGarryRead");
+            
             Person garry;
             using (var input = File.OpenRead(GarryFileName))
             {
@@ -48,6 +57,50 @@ namespace DNUG.ProtobufDemoComplete
             }
             
             Console.WriteLine($"{garry.Name} read from {GarryFileName}");
+        }
+        
+        private static void SaveAddressBook()
+        {
+            Console.WriteLine("SaveAddressBook");
+
+            var addressBook = AddressBookGenerator.Generate();
+            
+            var stopwatch = Stopwatch.StartNew();
+            using (var pbOutput = File.Create(AddressBookFileName))
+            {
+                addressBook.WriteTo(pbOutput);
+            }
+            Console.WriteLine($"Protobuf time: \t{stopwatch.ElapsedMilliseconds}ms");
+
+            
+            stopwatch.Restart();
+            using (var jsonOutput = File.CreateText(AddressBookJsonFileName))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(jsonOutput, addressBook);
+            }
+            Console.WriteLine($"JSON time: \t{stopwatch.ElapsedMilliseconds}ms");
+        }
+        
+        private static void ReadAddressBook()
+        {
+            Console.WriteLine("ReadAddressBook");
+            
+            var stopwatch = Stopwatch.StartNew();
+            using (var pbFile = File.OpenRead(AddressBookFileName))
+            {
+                AddressBook.Parser.ParseFrom(pbFile);
+            }
+            Console.WriteLine($"Protobuf time: \t{stopwatch.ElapsedMilliseconds}ms");
+
+            
+            stopwatch.Restart();
+            using (var jsonFile = File.OpenText(AddressBookJsonFileName))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Deserialize(jsonFile, typeof(AddressBook));
+            }
+            Console.WriteLine($"JSON time: \t{stopwatch.ElapsedMilliseconds}ms");
         }
     }
 }
